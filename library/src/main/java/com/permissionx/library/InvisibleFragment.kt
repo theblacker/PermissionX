44da703b11499f7e0c1b5a668f5846fa47d08797
liujh <liujh@nationalchip.com>
2026-03-16 08:33:03 +0800

    First commit

---
+++ library/src/main/java/com/permissionx/library/InvisibleFragment.kt
@@ -0,0 +1,43 @@
+package com.permissionx.library
+
+Typealias PermissionCallback = (Boolean, List<String>) -> Unit
+
+class InvisibleFragment : Fragment() {
+    private var callback : PermissionCallback? = null
+    fun requestNow(callback: PermissionCallback, vararg permissions: String) {
+        this.callback = callback
+        requestPermissions(permissions, 1)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            val deniedList = mutableListOf<String>()
+            grantResults.forEachIndexed { index, grantResult ->
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted = deniedList.isEmpty()
+            callback?.let { it(allGranted, deniedList) }
+        }
+    }
+}
+
+object PermissionX {
+    private const val TAG = "InvisibleFragment"
+    fun request(activity: FragmentActivity, vararg permissions: String, callback: PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+}
